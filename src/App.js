60dadef90cedebf74c82b8b546c941c289e3c454
Asad Ali Bhatti <asadalibhatti@hotmail.com>
2018-09-09 13:46:00 +0200

    adds projects routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
+import AppRoutes from "./components/routes/app_routes";
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <AppRoutes> </AppRoutes>
+      </div>
+    );
   }
 }
 
