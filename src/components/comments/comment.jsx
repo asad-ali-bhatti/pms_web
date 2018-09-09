28bfc866dc77f281fe27b37e8ac5ca95eedb3196
Asad Ali Bhatti <asadalibhatti@hotmail.com>
2018-09-09 13:22:22 +0200

    adds basic compnents without functionality

---
+++ src/components/comments/comment.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Comment extends Component {
+  render() {
+    return <h1>Comment Component</h1>;
+  }
+}
+
+export default Comment;
---
+++ src/components/tasks/task.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Task extends Component {
+  render() {
+    return <h1>Task Component</h1>;
+  }
+}
+
+export default Task;
